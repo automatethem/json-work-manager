fe08e375a61679f14d7e7504ae96889626c48d29
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-16 21:44:46 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def requirements():
 
 setuptools.setup(
 	name='json-worker',
-	version='0.0.2',
+	version='0.0.3',
 	description='Json worker',
 	long_description=open('README.md').read(),
 	long_description_content_type='text/markdown',
