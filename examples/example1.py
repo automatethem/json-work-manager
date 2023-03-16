480c7aa9d0e7a46fe23e7d34d9a20b5504747637
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-16 10:35:00 +0900

    Update example1.py

--- examples/example1.py
+++ examples/example1.py
@@ -6,26 +6,25 @@ from app.static_site_article_notification.static_site_article_notification impor
 import python_supporter
 import os
 import app
-
 from json_worker.json_worker import JsonWorker
 
-    def stop_callback(self):
-        print("stop")
-    
-    def end_callback(self):
-        print("end")
-    
-    def log_callback(self, message, verbose, background_rgb):
-        print(message, verbose, background_rgb)
-    
-    def start(self):
-        current_py_file_path = os.path.dirname(os.path.realpath(__file__))
-        inputs_directory = current_py_file_path+"/../inputs"
-        outputs_directory = current_py_file_path+"/../outputs"
-        config = python_supporter.config.load_config_from_file(inputs_directory+"/config.json")
-        worker_class = StaticSiteArticleNotification
-        #print(inputs_directory) #C:\Users\Administrator\Desktop\static-site-article-notification-app-main\app\app/../inputs
-        #print(outputs_directory) #C:\Users\Administrator\Desktop\static-site-article-notification-app-main\app\app/../outputs
-        
-        json_worker = JsonWorker(config, worker_class, inputs_directory, outputs_directory, lambda: print("stop"), lambda: print("end"), lambda message, verbose, background_rgb: print(message, verbose, background_rgb))
-        json_worker.start() 
+def stop_callback(self):
+    print("stop")
+
+def end_callback(self):
+    print("end")
+
+def log_callback(self, message, verbose, background_rgb):
+    print(message, verbose, background_rgb)
+
+def start(self):
+    current_py_file_path = os.path.dirname(os.path.realpath(__file__))
+    inputs_directory = current_py_file_path+"/../inputs"
+    outputs_directory = current_py_file_path+"/../outputs"
+    config = python_supporter.config.load_config_from_file(inputs_directory+"/config.json")
+    worker_class = StaticSiteArticleNotification
+    #print(inputs_directory) #C:\Users\Administrator\Desktop\static-site-article-notification-app-main\app\app/../inputs
+    #print(outputs_directory) #C:\Users\Administrator\Desktop\static-site-article-notification-app-main\app\app/../outputs
+
+    json_worker = JsonWorker(config, worker_class, inputs_directory, outputs_directory, lambda: print("stop"), lambda: print("end"), lambda message, verbose, background_rgb: print(message, verbose, background_rgb))
+    json_worker.start() 
