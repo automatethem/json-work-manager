465cca815ddf96cd8014daa4c756e67b8d0f1124
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-29 11:54:26 +0900

    Update json_work_manager.py

--- json_work_manager/json_work_manager.py
+++ json_work_manager/json_work_manager.py
@@ -87,9 +87,9 @@ class ThreadRun:
             self.work["internal_mark_work_done"] = True
 
 class JsonWorkManager:
-    def __init__(self, base_directory, json_work_class, stop_callback=None, end_callback=None, log_callback=None):
+    def __init__(self, base_directory, json_work_class, stop_callback=None, end_callback=None, log_callback=None, title = "Json 작업 관리자"):
         super().__init__()
-        self.title = "Json 작업 관리자"
+        self.title = title
         self.inputs_directory = base_directory + "/inputs"
         self.outputs_directory = base_directory + "/outputs"
         if not os.path.exists(self.outputs_directory):
