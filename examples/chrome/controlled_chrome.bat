423a79e47bddea8e85edcec5c98272e517a69796
Sang Ki Kwon <108699325+automatethem@users.noreply.github.com>
2023-03-30 17:22:51 +0900

    Create controlled_chrome.bat

---
+++ examples/chrome/controlled_chrome.bat
@@ -0,0 +1,1 @@
+start chrome "https://www.naver.com" --remote-debugging-port=1001 --remote-debugging-address=127.0.0.1 --user-data-dir=c:\user_data_dir\1001 --disk-cache-dir=null --disk-cache-size=0
